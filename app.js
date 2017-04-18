2c64d7d180e1686a33eda77138c8544e6daaa12b
Justin Goldby <goldbee500@gmail.com>
2017-04-17 23:10:34 -0700

    First Commit
    First Commit, website shell created, May use this as a templae for
    website that uses angular routes.

---
+++ app.js
@@ -0,0 +1,10 @@
+(function(){
+
+'use strict';
+
+//instantiate angular app and setup config for template routes
+angular
+.module('app', ['ngAnimate']);
+
+} //end function config
+})(); //end self invoking function.
