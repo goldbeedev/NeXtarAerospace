6975bacd9517e94ca748bf9a2cd7a123563f488b
Justin Goldby <goldbee500@gmail.com>
2017-05-29 23:08:14 -0700

    finished testing Nasa Photo of the day API, working on visual aspects as well.  Needs a lot of styling and functions to change what data the user is requesting from nasa

--- public/js/mainController.js
+++ public/js/mainController.js
@@ -3,14 +3,24 @@
 
 //initialize the angular module
 angular.module('app')
-.controller('mainController', ['$scope', '$location', function($scope, $location){
+.controller('mainController', ['$scope', '$location', '$http', function($scope, $location, $http){
 
 //set location to equal $location for data binding on the template
 $scope.$location = $location;
 
+function NasaData(){
+	$http.get("https://api.nasa.gov/planetary/apod?api_key=e9i490OQTmaJm70cRYYo5LiyhG9UWQ9j0Uxl8xoQ")
+	.then(function(response){
+		console.log(response);
+		$scope.test = response.data;
+		$scope.test2 = $scope.test.hdurl;
+		console.log($scope.test);
+		console.log($scope.test2);
+	}); //end function response
 
+} //end function NasaData
 
-
+NasaData();
 
 
 }]);	//end controller
