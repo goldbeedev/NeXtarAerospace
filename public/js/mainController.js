b73d8b689070f7ca5a73bf8c243ab20fe04281be
Justin Goldby <goldbee500@gmail.com>
2017-06-13 23:54:50 -0700

    messing around with the Nasa API, very bare bones still.  Action items: update the CSS to beautify the site.  Add functionality to use buttons to access different areas of the Nasa API

--- public/js/mainController.js
+++ public/js/mainController.js
@@ -3,7 +3,7 @@
 
 //initialize the angular module
 angular.module('app')
-.controller('mainController', ['$scope', '$location', '$http', function($scope, $location, $http){
+.controller('mainController', ['$scope', '$location', '$http', '$log', function($scope, $location, $http, $log){
 
 //set location to equal $location for data binding on the template
 $scope.$location = $location;
@@ -20,7 +20,16 @@ function NasaData(){
 
 } //end function NasaData
 
-NasaData();
+$scope.testfunc = function(){
+	$http.get("https://api.nasa.gov/planetary/earth/imagery?api_key=e9i490OQTmaJm70cRYYo5LiyhG9UWQ9j0Uxl8xoQ")
+	.then(function(response){
+		$scope.imagedata = response.data
+		console.log("this is the earth imagery data: " + $scope.imagedata);
+	}); //$http.get function(response)
+	console.log("test func working"); 
+} //end testfunc
+
+NasaData(); //calling the Nasa APOD api picture for testing purposes, may have this be the default API call upon page load.
 
 
 }]);	//end controller
